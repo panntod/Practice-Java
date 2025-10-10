cb9d946fee450bc8d9615e0df65ea8b1a4bc2307
panntod <munjalindra.pandhu@gmail.com>
2025-10-10 10:57:48 +0700

    feat: Quiz from College

---
+++ src/Quiz.java
@@ -0,0 +1,26 @@
+// SOAL GANJIL
+import java.util.Scanner;
+
+public class Quiz {
+    public static void main(String[] args) {
+     Scanner scan = new Scanner(System.in);
+
+     System.out.println("Masukan jumlah pinjaman: ");
+     int pinjaman = scan.nextInt();
+
+     System.out.println("Masukan lama pinjaman (bulan): ");
+     int lamaPinjaman = scan.nextInt();
+
+     double bunga = pinjaman * 0.01 * lamaPinjaman;
+     double totalPinjaman = pinjaman + bunga;
+     double cicilan = totalPinjaman / lamaPinjaman;
+
+     System.out.println("Cicilan per bulan: " + cicilan);
+    }
+}
+
+
+
+
+
+
